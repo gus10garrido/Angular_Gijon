76839c913c0997e12f63e7b3ea9510d1b84d9b90
gus10garrido <gus10garrido@gmail.com>
2017-11-14 17:27:05 +0100

    Comuniaciones

---
+++ p04-comunics/src/app/main/main.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HijoComponent } from './hijo/hijo.component';
+import { PadreComponent } from './padre/padre.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [HijoComponent, PadreComponent],
+  exports: [HijoComponent, PadreComponent]
+})
+export class MainModule { }
